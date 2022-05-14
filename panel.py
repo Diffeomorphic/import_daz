98bf2b688dc11084997ed6230c701484ea883cff
Thomas Comsol <thomas_larsson_01@hotmail.com>
2022-05-14 07:13:34 +0200

    Checking that new features don't crash Blender 2.93

--- panel.py
+++ panel.py
@@ -235,7 +235,8 @@ class DAZ_PT_AdvancedMaterials(DAZ_PT_Base, bpy.types.Panel):
         self.layout.operator("daz.import_daz_materials")
         self.layout.operator("daz.prune_node_trees")
         self.layout.separator()
-        self.layout.operator("daz.add_shell")
+        if bpy.app.version >= (3,1,0):
+            self.layout.operator("daz.add_shell")
         self.layout.operator("daz.replace_materials")
         self.layout.operator("daz.make_palette")
         self.layout.operator("daz.copy_materials")
