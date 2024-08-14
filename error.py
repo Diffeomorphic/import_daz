3eadb08da54c4625a2f1efaea809e27cb4e779bd
Thomas Comsol <thomas_larsson_01@hotmail.com>
2024-08-14 02:16:10 +0200

    License for one more file.

--- error.py
+++ error.py
@@ -14,8 +14,6 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
-
 import bpy
 from .settings import GS, LS
 from .utils import *
