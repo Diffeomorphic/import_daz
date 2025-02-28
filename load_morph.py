b101760bf0c9f6fe97625a04a1e92dfd56a00579
Thomas Comsol <thomas_larsson_01@hotmail.com>
2025-02-28 02:34:10 +0100

    Smoothstep bugfix.

--- load_morph.py
+++ load_morph.py
@@ -1376,6 +1376,7 @@ class LoadMorph(DriverUser):
             xi,yi = points[0]
             string = ""
             first = True
+            umax = 1
             for j,pt in enumerate(points[1:]):
                 xj,yj = pt
                 if yi == 0 and yj == 0:
