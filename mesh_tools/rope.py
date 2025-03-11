9793454cbfb644a28196c48bc032a1cc641eebfe
Thomas Comsol <thomas_larsson_01@hotmail.com>
2025-03-11 04:30:09 +0100

    Building curve from tube.

--- mesh_tools/rope.py
+++ mesh_tools/rope.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 import bpy
+import numpy as np
 from ..error import *
 from ..utils import *
 
@@ -18,7 +19,61 @@ class DAZ_OT_FindTube(DazOperator, IsMesh):
 
     def run(self, context):
         ob = context.object
-        print("OB", ob)
+        rings = self.findRings(ob)
+        print("RYY", rings)
+        strand = self.findCenters(rings, ob)
+        strands = [strand]
+        self.buildCurves(context, strands, ob)
+
+
+    def findRings(self, ob):
+        from ..tables import getVertEdges, otherEnd
+        vertedges = getVertEdges(ob)
+        ring = [v.index for v in ob.data.vertices if v.select]
+        rings = []
+        taken = []
+        while ring:
+            print("RR", ring)
+            rings.append(ring)
+            taken += ring
+            redges = [(vn, vertedges[vn]) for vn in ring]
+            ring = []
+            for vn,vedges in redges:
+                for e in vedges:
+                    vn2 = otherEnd(vn, e)
+                    if vn2 not in taken:
+                        ring.append(vn2)
+        return rings
+
+
+    def findCenters(self, rings, ob):
+        strand = []
+        alocs = np.array([tuple(v.co) for v in ob.data.vertices], dtype = float)
+        for ring in rings:
+            center = np.average(alocs[ring], axis=0)
+            strand.append(center)
+        return strand
+
+
+    def buildCurves(self, context, strands, ob):
+        r = 1.0*GS.scale
+        bpy.ops.curve.primitive_bezier_circle_add(radius=r)
+        bevel = context.object
+
+        cuname = "Center:%s" % ob.name
+        cu = bpy.data.curves.new(cuname, 'CURVE')
+        cu.dimensions = '3D'
+        cu.twist_mode = 'MINIMUM'
+        cu.bevel_mode = 'OBJECT'
+        cu.bevel_object = bevel
+        for strand in strands:
+            npoints = len(strand)
+            spline = cu.splines.new('POLY')
+            spline.points.add(npoints-1)
+            for co,point in zip(strand, spline.points):
+                point.co[0:3] = co
+        cuob = bpy.data.objects.new(cuname, cu)
+        context.collection.objects.link(cuob)
 
 #----------------------------------------------------------
 #   Initialize
