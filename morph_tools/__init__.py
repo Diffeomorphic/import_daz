40c92d60248f8844e439fdda9fb641d2a1b3d20c
Thomas Larsson <thomas.larsson@hdd.se>
2026-01-04 05:12:11 +0100

    Removed "Convert Morphs to Action".

--- morph_tools/__init__.py
+++ morph_tools/__init__.py
@@ -13,13 +13,11 @@ if DEBUG and "MorphFeature" in locals():
     import imp
     imp.reload(category)
     imp.reload(shapekeys)
-    imp.reload(morph_action)
     imp.reload(morph_panel)
 else:
     print("Loading Morph Tools")
     from . import category
     from . import shapekeys
-    from . import morph_action
     from . import morph_panel
     MorphFeature = True
 
@@ -30,19 +28,17 @@ else:
 def register():
     try:
         print("Register Morph Tools")
-        from . import category, shapekeys, morph_action, morph_panel
+        from . import category, shapekeys, morph_panel
         category.register()
         shapekeys.register()
-        morph_action.register()
         morph_panel.register()
     except (RuntimeError, ValueError):
         pass
 
 def unregister():
     try:
-        from . import category, shapekeys, morph_action, morph_panel
+        from . import category, shapekeys, morph_panel
         morph_panel.unregister()
-        morph_action.unregister()
         shapekeys.unregister()
         category.unregister()
     except (RuntimeError, ValueError):
