67e058d2ffa9b224c0b776145979c4161b9d1d0c
Thomas Larsson <thomas.larsson@hdd.se>
2026-07-02 09:26:06 +0200

    Merge uvs uses active graft material if no texture nodes are found.

--- merge_uvs.py
+++ merge_uvs.py
@@ -65,6 +65,8 @@ class UVLayerMerger:
                     dazRna(mat).DazShellMap = uvname
                     shellmaps[uvname] = True
 
+        if len(uvmaps.keys()) == 0 and active:
+            uvmaps[active.name] = True
         if len(uvmaps.keys()) == 1:
             return list(uvmaps.keys())[0]
         for uvname in uvmaps.keys():
