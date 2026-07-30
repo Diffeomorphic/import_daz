4314e1a687236633fc3c104b41b15a392bacc072
Thomas Larsson <thomas.larsson@hdd.se>
2026-07-30 13:21:45 +0200

    Fixed som errors in previous commit.

--- hair_tools/hair_builder.py
+++ hair_tools/hair_builder.py
@@ -295,16 +295,15 @@ class HairBuilder(Pinner, Collision, Cloth):
 
 
     def addDeformCurves(self, hair, hum):
-        from ..geonodes import addNodeGroup
+        from ..geonodes import addNodeGroup, setModSocket
         from .hair_nodes import DeleteInvalidGroup, DeformCurvesGroup
         if self.onInvalid != 'KEEP':
             modname = "DAZ Delete Invalid Curves"
             mod = hair.modifiers.new(modname, 'NODES')
             mod.node_group = addNodeGroup(DeleteInvalidGroup, modname, [])
             setModSocket(mod, 1, hum)
-            setModSocket(mod, 2, uvlayer.name)
-            socket = ("Input" if "Input_2" in mod.keys() else "Socket")
             uvlayer = hum.data.uv_layers.active
+            setModSocket(mod, 2, uvlayer.name)
             if self.onInvalid == 'REMOVE':
                 applyModifier(mod.name)
         modname = "DAZ Deform Curves"
