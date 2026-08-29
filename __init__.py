805061ecb75645dcba322f6bf6131f2fc0b2f4bc
Thomas Larsson <thomas.larsson@hdd.se>
2026-08-29 04:57:46 +0200

    Moved old-style property definitions back into propgroups.py, to avoid a crash.

--- __init__.py
+++ __init__.py
@@ -315,11 +315,6 @@ def register():
         if modname in Regnames:
             exec("%s.register()" % modname)
 
-    from .debug import OLD_STYLE_PROPS
-    if OLD_STYLE_PROPS:
-        from .old_propgroups import registerDazProperties
-        registerDazProperties()
-
     bpy.utils.register_class(DAZ_OT_EnableAllFeatures)
     bpy.utils.register_class(DAZ_OT_DisableAllFeatures)
 
